e8efdd0169dda7e253978efa38c7ead811e45282
Cristian Poll <cristian@cristianpoll.com>
2017-03-31 17:02:29 -0400

    add styling for component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,7 @@ export class Hero {
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css']
 })
 export class AppComponent {
     public  title = 'Tour of Heroes';
